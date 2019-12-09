195d624b7926329e7bf50d74db7974ce926026b6
GravityZ371 <41998962+GravityZ371@users.noreply.github.com>
2019-12-08 19:32:59 -0500

    broke the game

---
+++ RED!!!/Assets/Scripts/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    new Rigidbody rigidbody;
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // can thrust while rotating
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+          if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+}
